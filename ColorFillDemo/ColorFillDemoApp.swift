353c08c26399ea5e0196e59e33002ac3a51f17a5
Marcin Bartmiński <mbdjj@o2.pl>
2023-01-27 11:35:25 +0100

    Initial Commit

---
+++ ColorFillDemo/ColorFillDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorFillDemoApp.swift
+//  ColorFillDemo
+//
+//  Created by Marcin Bartminski on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorFillDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
